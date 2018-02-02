18a09a872ced60d3e4b1d6f288ae1260b9d45bb4
Chase Rivera <cmrivera353@gmail.com>
2018-02-02 13:46:54 -0600

    Changed blocks of spaces to tabs

--- src/SampleCabTest.java
+++ src/SampleCabTest.java
@@ -1,55 +1,55 @@
 import hw1.Cab;
 
 public class SampleCabTest {
-    public static void main(String[] args) {
-        // constructs a new cab with a base fare of $2.00
-        // and a per-mile rate of $3.00
-        Cab t = new Cab(3, 5);
+	public static void main(String[] args) {
+		// constructs a new cab with a base fare of $2.00
+		// and a per-mile rate of $3.00
+		Cab t = new Cab(3, 5);
 
-        // initially the total miles, total cash, current rate,
-        // and meter should be zero
-        System.out.println("Total Miles: " + t.getTotalMiles());
-        System.out.println("Expected 0");
-        System.out.println("Total Cash: " + t.getTotalCash());
-        System.out.println("Expected 0");
-        System.out.println("Current Rate: " + t.getCurrentRate());
-        System.out.println("Expected 0");
-        System.out.println("Meter: " + t.getMeter());
-        System.out.println("Expected 0");
-        System.out.println();
+		// initially the total miles, total cash, current rate,
+		// and meter should be zero
+		System.out.println("Total Miles: " + t.getTotalMiles());
+		System.out.println("Expected 0");
+		System.out.println("Total Cash: " + t.getTotalCash());
+		System.out.println("Expected 0");
+		System.out.println("Current Rate: " + t.getCurrentRate());
+		System.out.println("Expected 0");
+		System.out.println("Meter: " + t.getMeter());
+		System.out.println("Expected 0");
+		System.out.println();
 
-        // after driving 5 miles, total mileage should be 5
-        t.drive(5);
-        System.out.println("Total Miles: " + t.getTotalMiles());
-        System.out.println("Expected 5.0");
+		// after driving 5 miles, total mileage should be 5
+		t.drive(5);
+		System.out.println("Total Miles: " + t.getTotalMiles());
+		System.out.println("Expected 5.0");
 
-        // after pickUp, the meter should be 2.00 and the current rate should be 3.00
-        t.pickUp();  // a passenger gets in
-        System.out.println("Meter: " + t.getMeter());
-        System.out.println("Expected 2.0");
-        System.out.println("Current Rate: " + t.getCurrentRate());
-        System.out.println("Expected 3.0");
-        System.out.println();
+		// after pickUp, the meter should be 2.00 and the current rate should be 3.00
+		t.pickUp();  // a passenger gets in
+		System.out.println("Meter: " + t.getMeter());
+		System.out.println("Expected 2.0");
+		System.out.println("Current Rate: " + t.getCurrentRate());
+		System.out.println("Expected 3.0");
+		System.out.println();
 
-        // after driving 10 miles, the meter should be 32.00
-        t.drive(10);
-        System.out.println("Meter: " + t.getMeter());
-        System.out.println("Expected 32.0");
+		// after driving 10 miles, the meter should be 32.00
+		t.drive(10);
+		System.out.println("Meter: " + t.getMeter());
+		System.out.println("Expected 32.0");
 
-        // after dropOff, meter and current rate should be 0
-        t.dropOff();
-        System.out.println("Meter: " + t.getMeter());
-        System.out.println("Expected 0");
-        System.out.println("Current Rate: " + t.getCurrentRate());
-        System.out.println("Expected 0");
-        System.out.println();
+		// after dropOff, meter and current rate should be 0
+		t.dropOff();
+		System.out.println("Meter: " + t.getMeter());
+		System.out.println("Expected 0");
+		System.out.println("Current Rate: " + t.getCurrentRate());
+		System.out.println("Expected 0");
+		System.out.println();
 
-        // total cash collected should be 32.00
-        System.out.println("Total Cash: " + t.getTotalCash());
-        System.out.println("Expected 32.0");
+		// total cash collected should be 32.00
+		System.out.println("Total Cash: " + t.getTotalCash());
+		System.out.println("Expected 32.0");
 
-        // total miles should be 15
-        System.out.println("Total Miles: " + t.getTotalMiles());
-        System.out.println("Expected 15.0");
-    }
+		// total miles should be 15
+		System.out.println("Total Miles: " + t.getTotalMiles());
+		System.out.println("Expected 15.0");
+	}
 }
